3784cec29416746ac80760fd67a32abca93c8385
VighneshBirru <vignesh23birru@gmail.com>
2026-03-10 15:57:08 +0530

    added model_loader and utils

---
+++ research_and_analyst/__inti__.py
@@ -0,0 +1,8 @@
+"""
+research_and_analyst package.
+
+Keeping this file ensures Python treats the directory as a regular package
+(not just an implicit namespace package), which makes `python -m ...` and
+packaging behavior more predictable.
+"""
+
